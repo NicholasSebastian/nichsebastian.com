9b9182bba5cb5c4153c7c7fb9cb028a71f8975a3
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2020-10-20 22:12:35 +0700

    Added resume page

--- src/pages/resume.md
+++ src/pages/resume.md
@@ -1,19 +1,127 @@
 ---
 name: Nicholas Sebastian Hendrata
-website: nichsebastian.com
 email: nicholassebastianhendrata@gmail.com
 phone: "+62 812 8799 6551"
 phone2: "+65 9445 8310"
-linkedin: "linkedin.com/in/nichsebastian"
 nationality: "Indonesian w/ Singaporean NRIC"
 
-education:
-    - s
+experience: [
+    {
+        org: SIM IT Club,
+        role: "Subcommittee - Events",
+        year: "Oct 2020 - Present"
+    },
+    {
+        org: Bina Tunas Bangsa Student Council,
+        role: "Member of the IT Division",
+        year: "Aug 2016 - May 2017"
+    }
+]
+
+education: [
+    {
+        inst: University of Wollongong,
+        cert: "Bachelor of Computer Science, Mobile and Game Development",
+        year: "2019 - Present"
+    },
+    {
+        inst: University of London,
+        cert: "Certificate of Higher Education in Social Sciences",
+        year: "2018 - 2019",
+        note: "Graduated with merits."
+    },
+    {
+        inst: Bina Tunas Bangsa School,
+        cert: "International Baccalaureatte + National Exams",
+        year: "2007 - 2018"
+    }
+]
+
+certifications: [
+    {
+        name: Data Hackathon 2020,
+        org: SIM Data Analysis Club,
+        year: "February 2020",
+        note: "Awarded 3rd Place Overall and Best Demo"
+    },
+    {
+        name: IELTS,
+        org: British Council,
+        year: "January 2018",
+        note: "Scored 8.5 / 9"
+    }
+]
 
 skill:
-    - s
+    - Full Stack Web Development;
+    - Front-end Web Development with HTML, CSS, JavaScript, jQuery, and React.js.
+    - Back-end Web Development with Node.js and Express.
+    - iOS and Android Development with React Native.
+    - Game Development with C# and Unity or MonoGame.
 
 tools:
-    - s
+    - Visual Studio Code
+    - Eclipse IDE
+    - Visual Studio
+    - Adobe Photoshop
+    - iMovie
+    - Adobe XD / Figma
+    - Google Suite / MS Office
+    - Blender
 
+birth: "Jakarta, 11 Sepetember 2000"
+gender: Laki-laki
+religion: Buddha
+warga: Indonesia
+status: Single
+alamat: Pantai Indah Kapuk, Penjaringan, Jakarta Utara 14450
+
+pengalaman: Belum lulus.
+
+edukasi: [
+    {
+        inst: University of Wollongong,
+        cert: "Sarjana Komputer, Pengembangan Aplikasi Seluler dan Game",
+        year: "2019 - Sekarang"
+    },
+    {
+        inst: University of London,
+        cert: "Sertifikat Pendidikan Tinggi dalam Ilmu Sosial",
+        year: "2018 - 2019",
+        note: "Lulus dengan prestasi."
+    },
+    {
+        inst: SMA Bina Tunas Bangsa,
+        cert: "International Baccalaureatte + Ujian Nasional",
+        year: "2016 - 2018"
+    }
+]
+
+kemampuan:
+    - Mampu mengguna bahasa pemrograman JavaScript, C#, Python, Java dan C++.
+    - Pengembangan front-end Situs Web dengan HTML, CSS, JavaScript, jQuery dan React.js.
+    - Pengembangan back-end Situs Web dengan Node.js dan Express.
+    - Pemahaman database dengan MySQL dan MongoDB.
+    - Pengembangan aplikasi iOS dan Android dengan React Native.
+    - Pengembangan game dengan C# dan Unity Engine atau MonoGame.
+    - Pemahaman dasar UI/UX dan desain aplikasi dengan Adobe XD dan Figma.
+    - Pemahaman dasar pengeditan photo dengan Adobe Photohop.
+    - Pemahaman dasar pengeditan video dengan iMovie.
+    - Mampu penggunaan aplikasi-splikasi Microsoft Office dan Google Suite.
+    - Mampu bahasa Inggris (Aktif) dab bahasa Indonesia (Pasif).
+
+prestasi: [
+    {
+        name: Data Hackathon 2020,
+        org: SIM Klub Analisis Data,
+        year: "Februari 2020",
+        note: "Juara 3 dan Demo Terbaik"
+    },
+    {
+        name: IELTS,
+        org: British Council,
+        year: "Januari 2018",
+        note: "Scored 8.5 / 9"
+    }
+]
 ---
--- src/templates/resume.tsx
+++ src/templates/resume.tsx
@@ -1,34 +1,283 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React, { useState } from "react";
+import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
+import SEO from "../components/seo";
 
 const Resume = ({ data }) => {
+    const [lang, setLang] = useState<boolean>(true);
+    const { siteUrl, linkedin, github } = data.site.siteMetadata;
+    const photo = data.photo.childImageSharp.fixed;
     const {
-        name
+        name,
+        email,
+        phone,
+        phone2,
+        nationality,
+        experience,
+        education,
+        certifications,
+        skill,
+        tools,
+        birth,
+        gender,
+        religion,
+        status,
+        warga, 
+        alamat,
+        edukasi,
+        prestasi,
+        kemampuan
     } = data.file.childMarkdownRemark.frontmatter;
+
     return (
         <>
-            <h2>Resume Page</h2>
-            <div>{name}</div>
+            <SEO title="Resume" />
+            <div id="resume-nav">
+                <Link to="/">Home</Link>
+                <div>
+                    <button 
+                        onClick={() => setLang(true)}
+                        style={{ borderColor: lang ? '#fff' : '#000' }}>
+                        EN
+                    </button>
+                    <button 
+                        onClick={() => setLang(false)}
+                        style={{ borderColor: lang ? '#000' : '#fff' }}>
+                        ID
+                    </button>
+                </div>
+            </div>
+            {lang ?
+            <article id="resume">
+                <h3>{name}</h3>
+                <div className="col2">
+                    <div>
+                        <div>{siteUrl.substr(12)}</div>
+                        <div>{email}</div>
+                        <div>linkedin.com/in/{linkedin}</div>
+                        <div>github.com/{github}</div>
+                    </div>
+                    <div>
+                        <div>{nationality}</div>
+                        <div>{phone}</div>
+                        <div>{phone2}</div>
+                    </div>
+                </div>
+                <h4>Education</h4>
+                {education.map(edu => {
+                    return (
+                        <div>
+                            <div>{edu.inst}</div>
+                            <div>{edu.cert}</div>
+                            <div>{edu.year}</div>
+                            <div>{edu.note}</div>
+                        </div>
+                    );
+                })}
+                <div className="col2">
+                    <div>
+                        <h4>Skills</h4>
+                        <ul>{
+                            skill.map(sk => {
+                                return <li>{sk}</li>
+                            })
+                        }</ul>
+                    </div>
+                    <div>
+                        <h4>Tools</h4>
+                        <ul>{
+                            tools.map(tool => {
+                                return <li>{tool}</li>
+                            })
+                        }</ul>
+                    </div>
+                </div>
+                <div className="col2">
+                    <div>
+                        <h4>Awards and Certifications</h4>
+                        {certifications.map(cert => {
+                            return (
+                                <div>
+                                    <div>{cert.name}</div>
+                                    <div>{cert.org}</div>
+                                    <div>{cert.year}</div>
+                                    <div>{cert.note}</div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div>
+                        <h4>Experience</h4>
+                        {experience.map(exp => {
+                            return (
+                                <div>
+                                    <div>{exp.org}</div>
+                                    <div>{exp.role}</div>
+                                    <div>{exp.year}</div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </article>
+            :
+            <article id="resume-id">
+                <div className="col2">
+                    <div>
+                        <h3>{name}</h3>
+                        <table>
+                            <tbody>
+                                
+                                <tr>
+                                    <td>Tempat, Tanggal Lahir</td>
+                                    <td>{birth}</td>
+                                </tr>
+                                <tr>
+                                    <td>Jenis Kelamin</td>
+                                    <td>{gender}</td>
+                                </tr>
+                                <tr>
+                                    <td>Agama</td>
+                                    <td>{religion}</td>
+                                </tr>
+                                <tr>
+                                    <td>Status</td>
+                                    <td>{status}</td>
+                                </tr>
+                                <tr>
+                                    <td>Kewarganegaraan</td>
+                                    <td>{warga}</td>
+                                </tr>
+                                <tr>
+                                    <td>Telepon</td>
+                                    <td>{phone}</td>
+                                </tr>
+                                <tr>
+                                    <td>Situs</td>
+                                    <td>{siteUrl.substr(12)}</td>
+                                </tr>
+                                <tr>
+                                    <td>LinkedIn</td>
+                                    <td>linkedin.com/in/{linkedin}</td>
+                                </tr>
+                                <tr>
+                                    <td>E-mail</td>
+                                    <td>{email}</td>
+                                </tr>
+                                <tr>
+                                    <td>Alamat</td>
+                                    <td>{alamat}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <Img fixed={photo} />
+                </div>
+                <h4>Pendidikan Formal</h4>
+                {edukasi.map(edu => {
+                    return (
+                        <div>
+                            <div>{edu.inst}</div>
+                            <div>{edu.cert}</div>
+                            <div>{edu.year}</div>
+                            <div>{edu.note}</div>
+                        </div>
+                    );
+                })}
+                <h4>Keterampilan</h4>
+                <ul>{
+                    kemampuan.map(skill => {
+                        return <li>{skill}</li>;
+                    })
+                }</ul>
+                <h4>Prestasi</h4>
+                {prestasi.map(cert => {
+                    return (
+                        <div>
+                            <div>{cert.name}</div>
+                            <div>{cert.org}</div>
+                            <div>{cert.year}</div>
+                            <div>{cert.note}</div>
+                        </div>
+                    );
+                })}
+            </article>
+            }
         </>
     );
 }
-
-// Try scraping/API from linkedin instead.
 
 export default Resume;
 
 export const resumeQuery = graphql`
     query {
-        file (
-            name: {
-                eq: "resume"
+        site {
+            siteMetadata {
+                siteUrl
+                linkedin
+                github
             }
+        }
+        file (
+            relativePath: { eq: "resume.md" }
         ) {
             childMarkdownRemark {
                 frontmatter {
                     name
+                    email
+                    phone
+                    phone2
+                    nationality
+                    experience {
+                        org
+                        role
+                        year
+                    }
+                    education {
+                        inst
+                        cert
+                        year
+                        note
+                    }
+                    certifications {
+                        name
+                        org
+                        year
+                        note
+                    }
+                    skill
+                    tools
+                    birth
+                    gender
+                    religion
+                    warga
+                    status
+                    alamat
+                    edukasi {
+                        inst
+                        cert
+                        year
+                        note
+                    }
+                    prestasi {
+                        name
+                        org
+                        year
+                        note
+                    }
+                    kemampuan
                 }
             }
         }
+        photo:
+            file (
+                relativePath: { eq: "resume.jpg" }
+            ) {
+                childImageSharp {
+                    fixed(width: 180) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
     }
 `
